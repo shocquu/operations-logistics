8e5e550572c112e2a600631b4ee926a0ceb7c7e4
arkadiusz.liszka <arkadiusz.liszka@betmakers.com>
2022-06-05 02:10:48 +0200

    added algorithm

--- src/components/solutionTable.svelte
+++ src/components/solutionTable.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
     import { supply, demand, sellingPrices, purchasePrices, transportCosts } from '../stores/transport.store';
+    import { get } from 'svelte/store';
+    import { rowStore } from 'svelte-gantt/types/core/store';
 
-    export let customers;
-    export let suppliers;
+    const { _customers, _suppliers } = $$props;
+    const M: number = _suppliers.length;
+    const N: number = _customers.length;
 
-    const N = suppliers.length;
-    const M = customers.length;
+    let _supply = $supply;
+    let _demand = $demand;
 
-    $: _customers = customers;
-    $: _suppliers = suppliers;
-    $: _supply = $supply;
-    $: _demand = $demand;
-
-    const table = new Array(N).fill(0).map(() => new Array(M).fill(0));
+    const table = new Array(M).fill({ val: 0, rest: 0 }).map(() => new Array(N).fill({ val: 0, rest: 0 }));
 
     $: {
         $transportCosts;
@@ -25,9 +23,10 @@
     }
 
     const loop = () => {
-        for (let i = 0; i < N; i++) {
-            for (let j = 0; j < M; j++) {
-                table[i][j] = parseInt($sellingPrices[j]) - (parseInt($purchasePrices[i]) + $transportCosts[i][j]);
+        for (let i = 0; i < M; i++) {
+            for (let j = 0; j < N; j++) {
+                const val = parseInt($sellingPrices[j]) - (parseInt($purchasePrices[i]) + $transportCosts[i][j]);
+                table[i][j] = { val, rest: 0 };
             }
         }
     };
@@ -48,17 +47,112 @@
         }
 
         if (supplySum !== demandSum) {
-            _customers.push('OF');
-            _supply.push(demandSum.toString());
+            _customers[N] = 'OF';
+            _demand[N] = supplySum.toString();
 
-            _suppliers.push('DF');
-            _demand.push(supplySum.toString());
+            _suppliers[M] = 'DF';
+            _supply[M] = demandSum.toString();
 
-            console.log(supplySum, demandSum);
-
-            const row = [0, 0, 0, 0];
+            const row = [
+                { val: 0, rest: 0 },
+                { val: 0, rest: 0 },
+                { val: 0, rest: 0 },
+                { val: 0, rest: 0 },
+            ];
             table.push(row);
+            table.map((row) => {
+                row[N] = { val: 0, rest: 0 };
+            });
+
+            performStep1();
+            // setAlfaBeta();
         }
+    };
+
+    interface TableCell {
+        val: number;
+        rest: number;
+    }
+
+    const performStep1 = () => {
+        const values = [];
+
+        for (let i = 0; i < M + 1; i++) {
+            for (let j = 0; j < N + 1; j++) {
+                values.push({ item: table[i][j], i, j });
+            }
+        }
+
+        values.sort((a, b) => b.item.val - a.item.val);
+
+        // Push fictional dataset
+        // for (let i = 0; i < M + 1; i++) {
+        //     // for (let j = N; j < N + 1; j++) {
+        //     //     console.log(i, j);
+        //     // }
+        //     values.push({ item: table[M][i], i: M, j: i });
+        //     values.push({ item: table[i][N], i, j: N });
+        // }
+
+        // values.push([0, 0, 0, 0, 0, 0]);
+        console.log(values);
+
+        values.forEach((val) => {
+            let { item, i, j } = val;
+            let min = _supply[i] < _demand[j] ? parseInt(_supply[i]) : parseInt(_demand[j]);
+            // if (min > parseInt(_supply[i])) {
+            //     const remaining = min - parseInt(_supply[i]);
+            //     _supply[i + 1] = (parseInt(_supply[i + 1]) - remaining).toString();
+
+            //     item.rest = _supply[i];
+            //     table[i + 1][j].rest = remaining;
+            //     table[i + 1][j + 1].rest = parseInt(_supply[i + 1]);
+
+            //     _supply[i + 1] = (parseInt(_supply[i + 1]) - parseInt(_supply[i + 1])).toString();
+
+            //     let min2 = _supply[i + 1] < _demand[j + 1] ? parseInt(_supply[i + 1]) : parseInt(_demand[j + 1]);
+            //     console.log(min2);
+            //     return;
+            // }
+
+            // if (min > parseInt(_demand[j])) {
+            //     const remaining = min - parseInt(_demand[j]);
+            //     item.rest = _demand[j];
+            //     _demand[j + 1] = (parseInt(_demand[j + 1]) - remaining).toString();
+            //     return;
+            // }
+
+            if (j === N && i === M - 1) {
+                // const temp = i;
+                // i = j;
+                // j = temp;
+                // // min === 0 ? 'x' : min;
+                // table[i][j - 1].rest = table[i][j - 1].rest + min;
+                // table[2][2].rest = min;
+                // console.log(min, i, j);
+            } else {
+            }
+            item.rest = min === 0 ? 'x' : min;
+
+            _supply[i] = (parseInt(_supply[i]) - min).toString();
+            _demand[j] = (parseInt(_demand[j]) - min).toString();
+        });
+
+        // const allValues = [];
+        // const sortedRows = table.map((row, i) => {
+        //     return { item: row.sort((a, b) => b.val - a.val), i };
+        // });
+
+        // sortedRows.forEach((row, j) => {
+        //     allValues.push({ item: row.item, i: row.i, j });
+        // });
+
+        // allValues.sort((a, b) => b.val - a.val);
+    };
+
+    const setAlfaBeta = () => {
+        _customers[M + 1] = 'A<sup>i</sup>';
+        _suppliers[N + 1] = 'B<sup>i</sup>';
     };
 </script>
 
@@ -86,7 +180,12 @@
                         )
                     </th>
                     {#each _customers as _, j}
-                        <td class="px-3 py-2">{table[i][j]}</td>
+                        <td class="px-3 py-2">
+                            <div class="grid grid-rows-2 grid-cols-2">
+                                <p class="grid col-span-2">{table[i][j].val}</p>
+                                <p class="col-start-2">{table[i][j].rest}</p>
+                            </div></td
+                        >
                     {/each}
                 </tr>
             {/each}
--- src/routes/tp.svelte
+++ src/routes/tp.svelte
@@ -3,12 +3,14 @@
 
     let suppliers = ['D1', 'D2'];
     let customers = ['O1', 'O2', 'O3'];
+    let _suppliers = ['D1', 'D2'];
+    let _customers = ['O1', 'O2', 'O3'];
 </script>
 
 <main class="container mx-auto grid gap-5 h-[calc(100%-8rem)] grid-rows-[minmax(50px,_auto)_1fr]">
     <UserInput />
     <div class="flex gap-5">
         <TransportCostTable {customers} {suppliers} />
-        <SolutionTable {customers} {suppliers} />
+        <SolutionTable {_customers} {_suppliers} />
     </div>
 </main>
