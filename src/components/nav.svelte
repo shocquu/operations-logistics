b8e025b1b1e266e0d717ae92def01bd1beb2e7af
arkadiusz.liszka <arkadiusz.liszka@betmakers.com>
2022-06-06 03:29:07 +0200

    nav fix

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,20 +1,30 @@
 <script lang="ts">
-    import { Link } from 'svelte-navigator';
+    import { Link, useLocation } from 'svelte-navigator';
+
+    const location = useLocation();
+
+    const active = 'active text-blue-600 bg-gray-100';
+    const nonActive = 'text-gray-500 hover:text-gray-600 hover:bg-gray-50';
 </script>
 
 <ul class="flex flex-wrap justify-center border-b border-gray-200 max-w-xl mx-auto my-5">
     <li class="mr-2">
         <Link
-            to="tp"
-            class="inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg hover:text-gray-600 hover:bg-gray-50"
+            to="cpm"
+            aria-current="page"
+            class={`inline-block py-4 px-4 text-sm font-medium text-center rounded-t-lg ${
+                $location.pathname === '/cpm' ? active : nonActive
+            }`}
             >Critical Path Method
         </Link>
     </li>
     <li class="mr-2">
         <Link
-            to="cpm"
+            to="tp"
             aria-current="page"
-            class="inline-block py-4 px-4 text-sm font-medium text-center text-blue-600 bg-gray-100 rounded-t-lg active"
+            class={`inline-block py-4 px-4 text-sm font-medium text-center rounded-t-lg ${
+                $location.pathname === '/tp' ? active : nonActive
+            }`}
             >Transportation Problem
         </Link>
     </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     content: ['./src/**/*.svelte'],
     safelist: ['grid-cols-2', 'grid-cols-3', 'grid-cols-4', 'grid-cols-5', 'grid-cols-6'],
-    darkMode: false, // or 'media' or 'class'
+    media: false, // or 'media' or 'class'
     theme: {
         extend: {},
     },
